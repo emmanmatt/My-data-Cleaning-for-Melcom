1e0c55f9fb62134bb3e9c59ed2168e04aaaf5297
emmanmatt <141807986+emmanmatt@users.noreply.github.com>
2023-08-11 16:06:28 -0600

    Update and rename Melcom Data Cleaning.sql to Nashville Housing Data Cleaning.sql

--- Nashville Housing Data Cleaning.sql
+++ Nashville Housing Data Cleaning.sql
@@ -13,16 +13,20 @@ From PortfolioProject.dbo.NashvilleHousing
 -- Standardize Date Format
 
 
-Select PurchaseDateConverted, CONVERT(Date,PurchaseDate)
-From Melcom
+Select saleDateConverted, CONVERT(Date,SaleDate)
+From PortfolioProject.dbo.NashvilleHousing
 
 
-Update Melcom
-SET PurchaseDate = CONVERT(Date,PurchaseDate)
+Update NashvilleHousing
+SET SaleDate = CONVERT(Date,SaleDate)
 
 -- If it doesn't Update properly
 
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
 
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
 
 
  --------------------------------------------------------------------------------------------------------------------------
@@ -30,15 +34,15 @@ SET PurchaseDate = CONVERT(Date,PurchaseDate)
 -- Populate Property Address data
 
 Select *
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 --Where PropertyAddress is null
 order by ParcelID
 
 
 
 Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
-From Melcom a
-JOIN Melcom b
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 Where a.PropertyAddress is null
@@ -46,8 +50,8 @@ Where a.PropertyAddress is null
 
 Update a
 SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
-From Melcom a
-JOIN Melcom b
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 Where a.PropertyAddress is null
@@ -61,7 +65,7 @@ Where a.PropertyAddress is null
 
 
 Select PropertyAddress
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 --Where PropertyAddress is null
 --order by ParcelID
 
@@ -69,20 +73,20 @@ SELECT
 SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
 , SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
 
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 
 
-ALTER TABLE Melcom
+ALTER TABLE NashvilleHousing
 Add PropertySplitAddress Nvarchar(255);
 
-Update Melcom
+Update NashvilleHousing
 SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
 
 
-ALTER TABLE Melcom
+ALTER TABLE NashvilleHousing
 Add PropertySplitCity Nvarchar(255);
 
-Update Melcom
+Update NashvilleHousing
 SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
 
 
@@ -107,31 +111,31 @@ From PortfolioProject.dbo.NashvilleHousing
 
 
 
-ALTER TABLE Melcom
+ALTER TABLE NashvilleHousing
 Add OwnerSplitAddress Nvarchar(255);
 
-Update Melcom
+Update NashvilleHousing
 SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
 
 
-ALTER TABLE Melcom
+ALTER TABLE NashvilleHousing
 Add OwnerSplitCity Nvarchar(255);
 
-Update Melcom
+Update NashvilleHousing
 SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
 
 
 
-ALTER TABLE Melcom
+ALTER TABLE NashvilleHousing
 Add OwnerSplitState Nvarchar(255);
 
-Update Melcom
+Update NashvilleHousing
 SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
 
 
 Select *
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 
 
 
@@ -143,7 +147,7 @@ From Melcom
 
 
 Select Distinct(SoldAsVacant), Count(SoldAsVacant)
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 Group by SoldAsVacant
 order by 2
 
@@ -155,10 +159,10 @@ Select SoldAsVacant
 	   When SoldAsVacant = 'N' THEN 'No'
 	   ELSE SoldAsVacant
 	   END
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 
 
-Update Melcom
+Update NashvilleHousing
 SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 	   When SoldAsVacant = 'N' THEN 'No'
 	   ELSE SoldAsVacant
@@ -185,7 +189,7 @@ Select *,
 					UniqueID
 					) row_num
 
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 --order by ParcelID
 )
 Select *
@@ -196,7 +200,7 @@ Order by PropertyAddress
 
 
 Select *
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 
 
 
@@ -208,7 +212,7 @@ From Melcom
 
 
 Select *
-From Melcom
+From PortfolioProject.dbo.NashvilleHousing
 
 
 ALTER TABLE PortfolioProject.dbo.NashvilleHousing
@@ -224,6 +228,59 @@ DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
 
 
 
+
+
+
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+--- Importing Data using OPENROWSET and BULK INSERT	
+
+--  More advanced and looks cooler, but have to configure server appropriately to do correctly
+--  Wanted to provide this in case you wanted to try it
+
+
+--sp_configure 'show advanced options', 1;
+--RECONFIGURE;
+--GO
+--sp_configure 'Ad Hoc Distributed Queries', 1;
+--RECONFIGURE;
+--GO
+
+
+--USE PortfolioProject 
+
+--GO 
+
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
+
+--GO 
+
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
+
+--GO 
+
+
+---- Using BULK INSERT
+
+--USE PortfolioProject;
+--GO
+--BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
+--   WITH (
+--      FIELDTERMINATOR = ',',
+--      ROWTERMINATOR = '\n'
+--);
+--GO
+
+
+---- Using OPENROWSET
+--USE PortfolioProject;
+--GO
+--SELECT * INTO nashvilleHousing
+--FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
+--    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
+--GO
 
 
 
